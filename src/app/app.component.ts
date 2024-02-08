09f3058ec58049801ee38b3efac401a9754574ba
Aliel <leila.mouaci@gmail.com>
2024-02-08 15:57:18 +0100

    avancee css - reste a disparaitre les cartes si pas de saisie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { AdresseComponent } from './adresse/adresse.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MeteoComponent } from './meteo/meteo.component';
 import { CarteComponent } from './carte/carte.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormulaireComponent, AdresseComponent, HttpClientModule, MeteoComponent, CarteComponent],
+  imports: [RouterOutlet, FormulaireComponent, AdresseComponent, HttpClientModule, MeteoComponent, CarteComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
