b55a977a048152bae59d65647517ba15cc6d7152
Aliel <leila.mouaci@gmail.com>
2024-02-09 11:52:07 +0100

    zoom plus agrandi

--- src/app/carte/carte.component.ts
+++ src/app/carte/carte.component.ts
@@ -71,7 +71,7 @@ afficherCarte (axeX:number, axeY:number) {
   console.log("Axe Y arrivé dans afficherCate : " + axeY);
 
 
-    this.map = L.map('map').setView([axeY, axeX], 13);
+    this.map = L.map('map').setView([axeY, axeX], 12);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
